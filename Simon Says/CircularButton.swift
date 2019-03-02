3872c9ac12ab8829099616cbde4c3d729ff548f0
antmdev <executivesales@hotmail.co.uk>
2019-03-02 18:51:14 +0000

    initial commit

---
+++ Simon Says/CircularButton.swift
@@ -0,0 +1,21 @@
+//
+//  CircularButton.swift
+//  Simon Says
+//
+//  Created by Ant Milner on 02/03/2019.
+//  Copyright © 2019 Ant Milner. All rights reserved.
+//
+
+import UIKit
+
+class CircularButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
